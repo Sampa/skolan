876064f41b6d28083b31d06298f0c77c235b5d26
Sampa <idrini@gmail.com>
2016-08-21 15:49:40 +0200

    3.2

--- klient/3/3.2/sampa.js
+++ klient/3/3.2/sampa.js
@@ -1,5 +1,5 @@
 
 //Se ifall en sträng innehåller ordet "jag"
 var foo = "jag Jag jag";
-var result = foo.match(/Jag/ig);
-console.log(result);
+//var result = foo.match(/Jag/ig);
+//console.log(result);
