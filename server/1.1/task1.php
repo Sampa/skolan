0275432aab92aa1666a71ec23d910a994e9bfb2b
Sampa <idrini@gmail.com>
2017-03-29 10:42:41 +0200

    1.1 server side kollad

--- server/1.1/task1.php
+++ server/1.1/task1.php
@@ -5,9 +5,22 @@
  * Date: 2015-02-10
  * Time: 16:00
  */
-header('Content-type: text/plain');
-$countFile = "task1_count.txt";
-$count = file_get_contents($countFile)+1;
-echo $count;
-file_put_contents($countFile,$count);
+/*
+ * Öpnnar filen för att kunna läsa antalet besök och spara det i en variabel (+1 för det nyaste besöket)
+ * utan att nollställa filen först med file_get_contents
+ * flock() används för att låsa filen medans vi skriver in det nya antalet besök för att undvika krockar
+ * filen släpps och stängs sedan.
+ */
+//    header('Content-type: text/plain');
+    $path = "task1_count.txt";
+    $count = file_get_contents($path)+1;
+    echo $count;
+    $countFile = fopen($path,"w+");
+if(flock($countFile,LOCK_EX | LOCK_NB)){
+    fwrite($countFile,$count);
+    flock($countFile,LOCK_UN);
+}else{
+        echo "Kunde inte låsa filen";
+    }
+    fclose($countFile);
 ?>
